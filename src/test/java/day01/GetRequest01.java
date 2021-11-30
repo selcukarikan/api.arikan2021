4ec6bb4f6c17c2e1ed4ed8a8f573ca955bd1ab96
selcukarikan <selcukarikan1301@gmail.com>
2021-11-30 19:12:24 +0100

    dfd

--- src/test/java/day01/GetRequest01.java
+++ src/test/java/day01/GetRequest01.java
@@ -1,4 +1,10 @@
 package day01;
 
 public class GetRequest01 {
+
+    // https://restful-booker.herokuapp.com/booking/3 adresine bir request
+    // gonderildiginde donecek cevap(response) icin
+    // HTTP status kodunun 200
+    // Content Type nin Json
+    // status line nin HTTP7/1.1 200 OK oldugunu test ediniz
 }
