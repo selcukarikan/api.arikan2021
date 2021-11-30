5b814d8998decc5365c3cc264bf0dd4cfced8ef5
selcukarikan <selcukarikan1301@gmail.com>
2021-11-30 23:35:59 +0100

    assertThat

--- src/test/java/day01/GetRequest01.java
+++ src/test/java/day01/GetRequest01.java
@@ -1,6 +1,8 @@
 package day01;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import static io.restassured.RestAssured.given;
 
@@ -34,6 +36,24 @@ public class GetRequest01 {
 
     //5.dogrulama yap(assertion)
 
+                System.out.println("status Code: "+response.getStatusCode());
+                System.out.println("content Type : "+response.getContentType());
+                System.out.println("status Line : "+response.getStatusLine());
+                System.out.println("response.getHeaders() = " + response.getHeaders());
+
+              //  Assert.assertEquals(200,response.getStatusCode());
+
+                //expected kismi bize taslak olarak verilen degerdir, actual kismi ise response dan donen degerdir
+
+                //  Assert.assertEquals("application/json; charset=utf-8", response.getContentType());
+
+                //   Assert.assertEquals("HTTP/1.1 200 OK", response.getStatusLine());
+
+                response.then().
+                        assertThat().
+                        statusCode(200).
+                        contentType(ContentType.JSON).
+                        statusLine("HTTP/1.1 200 OK");
 
 }
 
