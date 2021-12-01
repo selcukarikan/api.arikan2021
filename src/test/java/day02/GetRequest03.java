acd36b0f1ec775b75d83fae7f5f223987919441b
selcukarikan <selcukarikan1301@gmail.com>
2021-12-01 23:19:57 +0100

    Matchers hasItem und hasSize

---
+++ src/test/java/day02/GetRequest03.java
@@ -0,0 +1,35 @@
+package day02;
+
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class GetRequest03 {
+    ///*
+    //    http://dummy.restapiexample.com/api/v1/employees url'ine
+    //    accept type'i "application/json" olan GET request'i yolladigimda
+    //    gelen response'un
+    //    status kodunun 200
+    //    ve content type'inin "application/json"
+    //   ve employees sayisinin 24
+    //   ve employee'lerden birinin "Ashton Cox"
+    //   ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin
+    //     */
+@Test
+            public void test03(){
+    String url = "http://dummy.restapiexample.com/api/v1/employees";
+    Response response = given().accept("application/json").when().get(url);
+    response.prettyPrint();
+
+    response.then().
+            assertThat().
+            statusCode(200).
+            contentType("application/json").
+            body("data.id", hasSize(24),
+                    "data.employee_name",hasItem("Ashton Cox"),
+                    "data.employee_age",hasItems(21,61,23));
+}
+}
