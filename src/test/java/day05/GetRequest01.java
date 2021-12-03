3dd081167424623946b892924e09480f1dc9c0aa
selcukarikan <selcukarikan1301@gmail.com>
2021-12-03 16:44:23 +0100

    jackson-mapper-asl

---
+++ src/test/java/day05/GetRequest01.java
@@ -0,0 +1,9 @@
+package day05;
+
+public class GetRequest01 {
+    // java da olusturdgumuz bir nesneyi veya sinifi,
+    // saklamak yada transfer etmek istedigimiz formata donusturme islemine serialization denir
+    // Bunun tam tersi duruma ise De-serialization denir. Yani API da donen response u Map , List,
+    //List of Map , Set gibi Java objelerine cevirme islemidir
+
+}
